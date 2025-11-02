3ff1f007c8a11bda1674e77b279bd6ebbf968bc8
Antonio Scaiella <37184273+piperino11@users.noreply.github.com>
2025-11-02 18:30:33 +0100

    Update __init__.py

--- src/babelnet_mcp/tools/__init__.py
+++ src/babelnet_mcp/tools/__init__.py
@@ -5,7 +5,11 @@ This module contains all MCP tools for interacting with BabelNet.
 """
 
 from .definition import register_definition_tool
+from .synset import register_synset_tools
+from .sense import register_sense_tools
 
 __all__ = [
-    "register_definition_tool"
+    "register_definition_tool",
+    "register_synset_tools",
+    "register_sense_tools"
 ]
