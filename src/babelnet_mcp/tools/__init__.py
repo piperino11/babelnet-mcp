42f5b289e57db225c86823597248709678d199bd
Antonio Scaiella <37184273+piperino11@users.noreply.github.com>
2025-11-02 17:39:31 +0100

    Create __init__.py

---
+++ src/babelnet_mcp/tools/__init__.py
@@ -0,0 +1,11 @@
+"""
+BabelNet MCP Tools
+
+This module contains all MCP tools for interacting with BabelNet.
+"""
+
+from .definition import register_definition_tool
+
+__all__ = [
+    "register_definition_tool"
+]
